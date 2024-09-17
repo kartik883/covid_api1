f36c75e74a8067cb798094e456841f8bc30ecd67
kartik883 <rajputkartik8448@gmail.com>
2024-09-18 00:56:57 +0530

    make countries page and search field to find countrie name by using api,textformfield, futurebuilder,listviewbuilder

--- lib/countries.dart
+++ lib/countries.dart
@@ -1,4 +1,6 @@
+import 'package:covid_tracker/service/stat_service.dart';
 import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
 
 class Countrieslist extends StatefulWidget {
   const Countrieslist({Key? key}) : super(key: key);
@@ -8,8 +10,119 @@ class Countrieslist extends StatefulWidget {
 }
 
 class _CountrieslistState extends State<Countrieslist> {
+  TextEditingController searchcontroller = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    statesService stateservice = statesService();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: TextFormField(
+                controller: searchcontroller,
+                onChanged: (value) {
+                  setState(() {});
+                },
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.symmetric(horizontal: 50),
+                  hintText: 'search with country name',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                ),
+              ),
+            ),
+            Expanded(
+                child: FutureBuilder(
+                    future: stateservice.countrieslist(),
+                    builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+                      if (!snapshot.hasData) {
+                        return ListView.builder(
+                            itemCount: 4,
+                            itemBuilder: (context, index) {
+                              return Shimmer.fromColors(
+                                baseColor: Colors.grey.shade700,
+                                highlightColor: Colors.grey.shade100,
+                                child: Column(
+                                  children: [
+                                    ListTile(
+                                      title: Container(
+                                        height: 10,
+                                        width: 9,
+                                        color: Colors.white,
+                                      ),
+                                      subtitle: Container(
+                                        height: 10,
+                                        width: 9,
+                                        color: Colors.white,
+                                      ),
+                                      leading: Container(
+                                        height: 10,
+                                        width: 9,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              );
+                            });
+                      } else {
+                        return ListView.builder(
+                            itemCount: snapshot.data!.length,
+                            itemBuilder: (context, index) {
+                              String name = snapshot.data![index]['country'];
+
+                              if (searchcontroller.text.isEmpty) {
+                                return Column(
+                                  children: [
+                                    ListTile(
+                                      title: Text(
+                                          snapshot.data![index]['country']),
+                                      subtitle: Text(
+                                          snapshot.data![index]['country']),
+                                      leading: Image(
+                                          height: 50,
+                                          width: 50,
+                                          image: NetworkImage(
+                                            snapshot.data![index]['countryInfo']
+                                                ['flag'],
+                                          )),
+                                    ),
+                                  ],
+                                );
+                              } else if (name.toLowerCase().contains(
+                                  searchcontroller.text.toLowerCase())) {
+                                return Column(
+                                  children: [
+                                    ListTile(
+                                      title: Text(
+                                          snapshot.data![index]['country']),
+                                      subtitle: Text(
+                                          snapshot.data![index]['country']),
+                                      leading: Image(
+                                          height: 50,
+                                          width: 50,
+                                          image: NetworkImage(
+                                            snapshot.data![index]['countryInfo']
+                                                ['flag'],
+                                          )),
+                                    ),
+                                  ],
+                                );
+                              } else {
+                                return Container();
+                              }
+                            });
+                      }
+                    })),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/service/stat_service.dart
+++ lib/service/stat_service.dart
@@ -14,4 +14,15 @@ class statesService {
       throw Exception('Error');
     }
   }
+
+  Future<List<dynamic>> countrieslist() async {
+    var data;
+    final response = await http.get(Uri.parse(AppUrl.countrieslist));
+    if (response.statusCode == 200) {
+      data = jsonDecode(response.body);
+      return data;
+    } else {
+      throw Exception('Error');
+    }
+  }
 }
